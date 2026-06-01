5c4dd9c3e05e6d8d82b246c0fb5f8dfe35c7d719
Rmxpvl <remy.pinville@gmail.com>
2026-06-01 16:45:22 +0200

    1st project

--- sql_crud/0-create_table.sql
+++ sql_crud/0-create_table.sql
@@ -1,4 +1,9 @@
-CREATE TABLE IF NOT EXISTS first_table (
-	id INT,
-	name VARCHAR(256)
+CREATE TABLE books (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    author TEXT NOT NULL,
+    genre TEXT NOT NULL,
+    price REAL,
+    stock INTEGER CHECK (stock >= 0),
+    published_year INTEGER
 );
